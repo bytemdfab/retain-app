46454156609d858f4155951a35242e89ecd7458d
byte.mdfab <byte.mdfab@gmail.com>
2016-08-15 23:22:23 +0300

    Conditional showing of the icon

--- src/app/ui/note-card.ts
+++ src/app/ui/note-card.ts
@@ -34,8 +34,12 @@ import {Component, Input} from "@angular/core";
   cursor: pointer; 
 }`],
     template: `
-<div class="note-card row shadow-1">
-  <div class="icon" (click)="onChecked()">
+<div 
+        class="note-card row shadow-1"
+        (mouseenter)="toggle()"
+        (mouseleave)="toggle()"
+>
+  <div class="icon" *ngIf="showCheck" (click)="onChecked()">
     <i class="material-icons">check</i>
   </div>
   <div class="col-xs-12 title">
@@ -48,6 +52,12 @@ import {Component, Input} from "@angular/core";
 })
 export class NoteCard {
     @Input('note') note = {};
+
+    showCheck: boolean = false;
+
+    toggle() {
+        this.showCheck = !this.showCheck;
+    }
 
     onChecked() {
         console.log('click')
