7f30c9f7fd6b7c170a5801809290c424e94d7381
byte.mdfab <byte.mdfab@gmail.com>
2016-08-15 22:19:43 +0300

    Container component

---
+++ src/app/containers/main.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'main-container',
+	template: `
+	<div>
+		<main  class="main">
+			Content goes here
+		</main>
+	</div>
+	`
+})
+export class Main {
+	
+}
