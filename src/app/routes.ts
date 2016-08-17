f0f6d3002375a794e18706c3fa0619d9519fb0cd
byte.mdfab <byte.mdfab@gmail.com>
2016-08-17 23:30:06 +0300

    Add auth to routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
 import {RouterConfig} from '@angular/router';
-import {Notes, Main, About} from './containers';
+import {Notes, Main, About, Auth} from './containers';
+import {AuthService} from './services';
 
 export const routes: RouterConfig = [
     {
         path: '',
         component: Main,
+        canActivate: [AuthService],
         children: [
             {
                 path: '',
@@ -15,6 +17,10 @@ export const routes: RouterConfig = [
                 component: About
             }
         ]
+    },
+    {
+        path: 'auth',
+        component: Auth
     },
     {
         path: '**',
